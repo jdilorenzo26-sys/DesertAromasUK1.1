594fd6580b73e86498c36506ceb730f82e51ac2d
jdilorenzo26-sys <jdilorenzo26@gmail.com>
2025-08-22 20:07:34 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,16 @@ export default function Home() {
       <section className="text-center py-20">
         <h1 className="text-4xl md:text-5xl font-bold">Luxury Inspired Perfumes</h1>
         <p className="mt-4 text-lg max-w-2xl mx-auto opacity-80">Discover timeless Arabic-inspired scents crafted with elegance.</p>
-        <div className="mt-8">
-          <Link className="btn" href="/collections">Samples</Link>
-          <Link className="btn" href="/collections">Samples</Link>
-        </div>
+        <div className="mt-8 flex justify-center">
+  <ul className="flex flex-col space-y-3">
+    <li>
+      <Link className="btn" href="/collections">Samples</Link>
+    </li>
+    <li>
+      <Link className="btn" href="/collections">Full Collection</Link>
+    </li>
+  </ul>
+</div>
       </section>
     </main>
   )
