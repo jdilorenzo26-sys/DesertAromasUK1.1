a3cee5140e29a150941cdce6431a9192e4930878
jdilorenzo26-sys <jdilorenzo26@gmail.com>
2025-08-23 00:41:34 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,3 +17,16 @@ module.exports = {
   },
   plugins: [],
 };
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide') // 👈 add this line
+  ],
+}
