f31fef69564059f1de007cab006a6eccc2dab381
jdilorenzo26-sys <jdilorenzo26@gmail.com>
2025-08-23 01:01:03 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,3 +30,20 @@ module.exports = {
     require('tailwind-scrollbar-hide') // 👈 add this line
   ],
 }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
